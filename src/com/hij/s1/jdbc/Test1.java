d09ff5fcba08b6cecb74d47b434d97e3c3b66c98
injunyee <ghkd306@gmail.com>
2022-01-19 12:24:15 +0900

    1

---
+++ src/com/hij/s1/jdbc/Test1.java
@@ -0,0 +1,14 @@
+package com.hij.s1.jdbc;
+import java.util.*;
+
+public class Test1
+{
+	public static void main(String args[])
+	{
+		System.out.printf("hellow");
+		String st;
+		Scanner sc = new Scanner(System.in);
+		st = sc.next();
+		System.out.printf("%s\n", st);
+	}
+}
