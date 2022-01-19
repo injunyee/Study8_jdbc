3ab07071252572254b14cddd46d92d4c3b53d8ea
injunyee <ghkd306@gmail.com>
2022-01-19 17:49:48 +0900

    4

--- src/com/hij/location/LocationDAO.java
+++ src/com/hij/location/LocationDAO.java
@@ -1,9 +1,32 @@
 package com.hij.location;
+import java.util.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.hij.util.DBConnector;
 
 public class LocationDAO
 {
-	public void getList()
+	public void getList() throws ClassNotFoundException, SQLException
 	{
+		DBConnector dbConnector = new DBConnector();
+		Connection con = dbConnector.getConnect();
+		String sql = "SELECT * FROM LOCATIONS";
+		PreparedStatement pst = con.prepareStatement(sql);
+		ResultSet rs = pst.executeQuery();
 		
+		while(rs.next())
+		{
+			LocationDTO locationDTO = new LocationDTO();
+			locationDTO.setLocation_id(rs.getString("location_id"));
+			locationDTO.setStreet_address(rs.getString("street_address"));
+			locationDTO.setPostal_code(rs.getString("postal_code"));
+		}
+		
+		rs.close();
+		pst.close();
+		con.close();
 	}
 }
